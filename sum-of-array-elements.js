a6eef7a0a3f5e47e3e3d7a6df6696c8f98b93b1d
Your Name <you@example.com>
2026-02-26 21:04:04 +0600

    declaire function and check a number is odd or even

---
+++ sum-of-array-elements.js
@@ -0,0 +1,21 @@
+function isEven (number){
+    if (number%2===0){
+        return true;
+
+    }else{
+        return false;
+    }
+}
+let result= isEven(22);
+console.log(result);
+
+function isOdd(num){
+    if(num%2===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+} 
+let result_of_num= isOdd(25)
+console.log(result_of_num);
